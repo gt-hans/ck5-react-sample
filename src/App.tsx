0ea545a0bec51630d15942563ce5d9e7ad0ee804
Hans Wong <166590402+rr-hans@users.noreply.github.com>
2024-06-07 17:13:59 +0800

    added ckeditor5 and work

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ClassicEditor from "ckeditor5-custom-build/build/ckeditor";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppState {
+  editorInstance: any | null;
+}
+
+class App extends Component {
+  state: AppState = {
+    editorInstance: null,
+  };
+  handleEditorReady = (editor: typeof ClassicEditor) => {
+    this.setState({ editorInstance: editor });
+  };
+
+  handleExportHtml = () => {
+    const { editorInstance } = this.state;
+    if (editorInstance) {
+      const htmlContent = editorInstance.getData();
+      console.log(htmlContent); // Or handle the export as needed
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <h2>Using CKEditor&nbsp;5 build in React</h2>
+        <CKEditor
+          editor={ClassicEditor.Editor}
+          data="<p>Hello from CKEditor&nbsp;5!</p>"
+          onReady={(editor) => {
+            // You can store the "editor" and use when it is needed.
+            this.setState({ editorInstance: editor });
+            console.log("Editor is ready to use!", editor);
+          }}
+          onChange={(event, editor) => {
+            console.log(event);
+          }}
+          onBlur={(event, editor) => {
+            console.log("Blur.", editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log("Focus.", editor);
+          }}
+        />
+        <button onClick={this.handleExportHtml}>Export HTML</button>
+      </div>
+    );
+  }
 }
 
 export default App;
